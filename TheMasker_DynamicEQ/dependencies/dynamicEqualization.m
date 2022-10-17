e2be72556e40115a20e5ef7840fee1fa574e0381
Amedeo Fresia <amedeofresia@gmail.com>
2022-10-17 10:18:06 +0200

    pultio una parte di codice

--- TheMasker_DynamicEQ/dependencies/dynamicEqualization.m
+++ TheMasker_DynamicEQ/dependencies/dynamicEqualization.m
@@ -5,8 +5,8 @@ function wetSignal = dynamicEqualization(blockIN_Gain, threshold, nfft, fs, nfil
     
     %converting from frquency to Barks
     %Based on https://stackoverflow.com/questions/10754549/fft-bin-width-clarification
-    bin_width = fs / nfft
-    %invece i valori di bins dove li abbiamo visti insieme?
+    bin_width = fs / nfft;
+    %questi valori di bins dove li abbiamo visti insieme?
     %sono giusti i valori da 0 a 256? o devono essere 512?
     bins=(0:bin_width:fs/2-1);
     
