4a0598817cea7f24c42fdf39f921f8cbf074d8ff
Amedeo Fresia <amedeofresia@gmail.com>
2022-10-23 23:35:07 +0200

    added plot of in, th and delta, problems on input db values

--- TheMasker_DynamicEQ/getDelta.m
+++ TheMasker_DynamicEQ/getDelta.m
@@ -2,7 +2,11 @@ function deltaSignal = getDelta(inputSignal, threshold)
 
     deltaSignal = inputSignal - threshold;
 
-    p = plot(linspace(1,length(deltaSignal),length(deltaSignal)) ,(deltaSignal(:).'));
+    %plot threshold and delta
+    THplot = plot(linspace(1,length(threshold),length(threshold)) ,(threshold(:).'), 'red');
     hold on;
+    INplot = plot(linspace(1,length(inputSignal),length(inputSignal)) ,(inputSignal(:).'), 'blue');
+    hold on;
+    Dplot = plot(linspace(1,length(deltaSignal),length(deltaSignal)) ,(deltaSignal(:).'), 'green');
 
 end
